c220e9786f8df53e6cabf05315cbaaa427eef849
Akim Soulé <soule_akim@yahoo.fr>
2026-01-16 23:57:55 -0500

    You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@xenova/transformers",
+      "onnxruntime-node",
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { "onnxruntime-node": false };
+    return config;
+  },
 };
 
 export default nextConfig;
